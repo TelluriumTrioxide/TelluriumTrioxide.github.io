740b853998e2ccb2778d84da0fcb4018b970bbb5
TelluriumTrioxide <teo.silfverhjelm@gmail.com>
2025-03-02 23:12:23 +0100

    asd

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 
 function SetSectionHeight(div, offset, ...args) {
-    var section = document.getElementById(div);
- 
     for (var id of args)
     {
         var elem = document.getElementById(id);
         total += elem.offsetHeight;
     }
-    section.style.minHeight = (total - offset) + "px"; 
+    document.getElementById(div).style.minHeight = (total - offset) + "px"; 
 }
