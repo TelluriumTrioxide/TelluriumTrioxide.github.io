664fd2cb68882dd51bfdf90902ae70b4e6a31acf
TelluriumTrioxide <teo.silfverhjelm@gmail.com>
2025-03-03 01:26:04 +0100

    asd

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 
-function SetSectionHeight(div, offset, ...args) {
+function SetSectionHeight(div, inoffset, ...args) {
     var total = 0;
+    var lastelem;
     for (var id of args)
     {
         var elem = document.getElementById(id);
         total += elem.offsetHeight;
         total += parseInt(window.getComputedStyle(elem).getPropertyValue('margin-top'));
         total += parseInt(window.getComputedStyle(elem).getPropertyValue('margin-bottom'));
+        lastelem = elem;
     }
-    document.getElementById(div).style.minHeight = (total - offset) + "px"; 
+    var offset = 0;
+    offset += elem.offsetHeight;
+    offset += parseInt(window.getComputedStyle(elem).getPropertyValue('margin-top'));
+    offset += parseInt(window.getComputedStyle(elem).getPropertyValue('margin-bottom'));
+    offset *= 0.5;
+    document.getElementById(div).style.minHeight = (total - offset-inoffset) + "px"; 
 }
